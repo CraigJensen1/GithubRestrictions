89bdc2160f1cb18e4f28895c95b47f1b1e7ba83f
craig.jensen <craig.jensen1@snow.edu>
2025-09-08 14:04:11 -0600

    Push this awenessness

--- GithubRestrictions/Program.cs
+++ GithubRestrictions/Program.cs
@@ -1,3 +1,32 @@
-﻿using System;
-class pl { public void adds(string n) { Console.WriteLine("Added: " + n); } }
-class program { static void Main() { pl p = new pl();p.adds("Song A"); } }
+﻿// Don't abbreviate ever
+// Crappy vague variable name
+// PascalCase = Methods, classes etc.
+// Variable are in camelCase
+
+
+class PlayList
+{
+
+    public const decimal UTAH_TAX_CODE = 343.343m;
+    public const decimal PI = 3.14m;
+
+
+    public void ThisIsAddingASong(string name)
+    {
+        Console.WriteLine("Added: " + name);
+
+        if (true)
+        {
+            Console.WriteLine("indent");
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        PlayList playList = new PlayList();
+        playList.AddSong("Song A");
+    }
+}
