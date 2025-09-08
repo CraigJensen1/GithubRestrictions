0662db317f389e4c08c627681bb3b06e27bdb2aa
jonathan.owen <jonathan.owen@students.snow.edu>
2025-09-08 14:06:36 -0600

    fixed code format

--- GithubRestrictions/Program.cs
+++ GithubRestrictions/Program.cs
@@ -1,3 +1,25 @@
 ﻿using System;
-class pl { public void adds(string n) { Console.WriteLine("Added: " + n); } }
-class program { static void Main() { pl p = new pl();p.adds("Song A"); } }
+
+
+
+class Playlist 
+{
+
+    public const decimal UTAH_TAX_CODE = 343.343m;
+    public const decimal PI = 3.14m;
+
+
+    public void AddingASong(string name) 
+    { 
+        Console.WriteLine("Added: " + name); 
+    } 
+}
+
+class Program 
+{ 
+    static void Main() 
+    { 
+        Playlist playList = new Playlist();
+        playList.AddSong("Song A"); 
+    } 
+}
